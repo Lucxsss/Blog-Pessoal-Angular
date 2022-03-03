79c5e513a0d118aedb08c729b18e78033b16e551
Lucas <watchlucas274@gmail.com>
2022-03-03 14:06:37 -0300

    @Lucxsss[Component inicio/tema]

--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment.prod';
 import { User } from '../model/User';
 import { UserLogin } from '../model/UserLogin';
 
@@ -9,19 +10,30 @@ import { UserLogin } from '../model/UserLogin';
 })
 export class AuthService {
 
-  constructor( 
-        private http:HttpClient
+  constructor(
+    private http: HttpClient
   ) { }
 
 
-  entrar(userLogin: UserLogin): Observable<UserLogin>{ 
-          return this.http.post<UserLogin>("https://bloglucao.herokuapp.com/usuarios/logar", userLogin)
+  entrar(userLogin: UserLogin): Observable<UserLogin> {
+    return this.http.post<UserLogin>("https://bloglucao.herokuapp.com/usuarios/logar", userLogin)
 
-  }                                         
-
-  cadastrar(user: User): Observable<User>{ 
-          return this.http.post<User>("https://bloglucao.herokuapp.com/usuarios/cadastrar", user)
   }
 
+  cadastrar(user: User): Observable<User> {
+    return this.http.post<User>("https://bloglucao.herokuapp.com/usuarios/cadastrar", user)
+  }
 
-}
+  logado() {
+    let ok: boolean = false
+
+
+    if (environment.token != '') {
+      ok = true
+
+    }
+
+    return ok
+
+  }
+} 
